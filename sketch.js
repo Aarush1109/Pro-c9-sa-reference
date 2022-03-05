eea975a8b7689cca2a06a3e8949299601feac057
Aarush1109 <100951373+Aarush1109@users.noreply.github.com>
2022-03-05 21:58:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,48 @@
-var box;
 
 function setup() {
-  createCanvas(400,400);
-  box = createSprite(200,200,30,30);
-
+  createCanvas(600,600);
+  background(51);
+  box = createSprite(300,300,30,30);
+  box.shapeColor = "black";
 }
 
 function draw() 
 {
-   background(30);
 
+  if (keyDown("left")){
+    box.position.x -=5;
+    }
+  if (keyDown("right")){
+      box.position.x +=5;
+    }
+  if (keyDown("up")){
+      box.position.y -=5;
+    }
+  if (keyDown("down")){
+      box.position.y +=5;
+    }
   if (keyIsDown(RIGHT_ARROW)) 
   {
-    box.position.x = box.position.x +5;
+    background("yellow");
+    
   }
-
-    if (keyIsDown(LEFT_ARROW)) 
+  if (keyIsDown(LEFT_ARROW)) 
   {
-    box.position.x= box.position.x -5;
+    background("blue");
     
   }
  
     if (keyIsDown(UP_ARROW)) 
   {
-    box.position.y = box.position.y - 5;
+    background("red");
    
   }
 
   if (keyIsDown(DOWN_ARROW)) 
   {
-    box.position.y = box.position.y + 5;
+    background("green");
   }
-
+  
   drawSprites();
 }
-
-
-
 
